ef99d0775a2bda2d0d6c12f200e095b18cd9e755
initdaemon <aadityarautela@protonmail.com>
2021-06-06 17:43:59 +0530

    Fixed a Stupid Error

--- pytorchyolo/train.py
+++ pytorchyolo/train.py
@@ -74,7 +74,7 @@ def run():
     parser.add_argument("--nms_thres", type=float, default=0.5, help="Evaluation: IOU threshold for non-maximum suppression")
     parser.add_argument("--logdir", type=str, default="logs", help="Directory for training log files (e.g. for TensorBoard)")
     parser.add_argument("--seed", type=int, default=-1, help="Makes results reproducable. Set -1 to disable.")
-    parser.add_argument("--checkpoint_basepath", type='str', default='checkpoints/', help='Checkpoint Location')
+    parser.add_argument("--checkpoint_basepath", type=str, default='checkpoints/', help='Checkpoint Location')
     args = parser.parse_args()
     print(f"Command line arguments: {args}")
 
